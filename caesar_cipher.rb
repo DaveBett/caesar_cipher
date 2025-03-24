a8703fbe594604c499be7e46f4609d6dc9926a4e
DaveBett <davide.betterini@gmail.com>
2025-03-24 11:17:00 +0000

    polished code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,53 +1,59 @@
-def caesar_cipher(string, modifier)
-  #takes in a string and the shift factor and then outputs the modified string using a right shift
-  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
-  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# frozen_string_literal: true
 
-  cryptid = Array.new
+require 'rubocop-performance'
+
+def caesar_cipher(string, modifier)
+  # takes in a string and the shift factor and then outputs the modified
+  # string using a right shift
+  alphabet = %w[a b c d e f g h i j k l m n o
+                p q r s t u v w x y z]
+
+  cryptid = []
   key = modifier.to_i
 
-  #reduces number of loops around the alphabet for number exceeding 26
-  while key > 26
-    key -= 26
-  end
+  # reduces number of loops around the alphabet for number exceeding 26
+  key -= 26 while key > 26
 
-  #makes the modifier go down the alphabet
+  # makes the modifier go down the alphabet
   mod = key - 26
 
-  for i in 0...string.length do
-    #iterates through each letter of the string
-    
+  (0...string.length).each do |i|
+    # iterates through each letter of the string
+
     string_letter = string[i].chr
     alphabet.each_with_index do |letter, index|
-
       if alphabet.include?(string_letter) == false
-        #checks if the current character is not in the alphabet array, this includes capital letters
+        # checks if the current character is not in the alphabet array,
+        # this includes capital letters
 
         if letter.upcase == string_letter && alphabet.include?(string_letter.downcase)
-          #checks if the current character is capitalized and adds it to the cryptid array if it is
+          # checks if the current character is capitalized and adds it
+          # to the cryptid array if it is
           cryptid[i] = alphabet[index + mod].upcase
           break
         elsif !alphabet.include?(string_letter.downcase)
-          #checks if the current character is not a letter and adds it to the cryptid array if it is
+          # checks if the current character is not a letter and adds it
+          # to the cryptid array if it is
           cryptid[i] = string_letter
           break
         end
 
       elsif letter == string_letter
-        #checks if the current character is a letter and adds it to the cryptid array if it is
+        # checks if the current character is a letter and adds it
+        # to the cryptid array if it is
         cryptid[i] = alphabet[index + mod]
         break
       end
     end
   end
-  return cryptid.join
+  cryptid.join
 end
 
-print "What would you like to encrypt? "
+print 'What would you like to encrypt? '
 string = gets.chomp
 
-print "Input the number of shifts for each letter: "
+print 'Input the number of shifts for each letter: '
 shift = gets.chomp
 
-print "Cipher Text: "
+print 'Cipher Text: '
 puts caesar_cipher(string, shift)
